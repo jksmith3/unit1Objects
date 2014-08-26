27a0671219650440bcdd03e37b7c05bcd67a437c
jksmith3 <jksmith@student.naperville203.org>
2014-08-26 12:18:21 -0500

    Completed Chapter 1 practice activities
    Used URL and Dialog boxes and titles

---
+++ ScrewingUp/DialogViewer.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+public class DialogViewer
+{
+   public static void main(String[] args)
+   {
+      String name = JOptionPane.showInputDialog("what is your name");
+      System.out.println(name);
+   }
+}
